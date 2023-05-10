d7e14f52efe0f6ffc215cf0978dd138e7f0be666
Uttam Kumaran <uttam17@users.noreply.github.com>
2023-05-10 16:16:41 -0500

    Updating file

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,14 @@
-## Write a file that contains the text "Hello World!"
-with open("hello_world.txt", "w") as f:
-    f.write("Hello World!")
+## Write a simple few functions that demonstrate pythons math capabilities  
+def add(a, b):    
+   return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    return a / b
+def power(a, b):
+    return a ** b
+def square_root(a):
+    return a ** 0.5
     
